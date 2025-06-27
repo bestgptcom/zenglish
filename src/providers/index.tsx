e4531f70993b08ac09bf3f0eb76e8cc6c9e03e69
Catalin <151016750+bestgptcom@users.noreply.github.com>
2025-06-27 16:41:28 +0600

    Update index.tsx

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import { HeroUIWrapper } from '@/providers/HeroUIWrapper';
+import React from "react";
+import { HeroUIWrapper } from "@/providers/HeroUIWrapper";
+import { PopupProvider } from "@/contexts/PopupContext";
 
-export default function MainProvider({ children }: { children: React.ReactNode }) {
-    return (
-        <HeroUIWrapper>
-            {children}
-        </HeroUIWrapper>
-    );
+export default function MainProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <HeroUIWrapper>
+      <PopupProvider>{children}</PopupProvider>
+    </HeroUIWrapper>
+  );
 }
