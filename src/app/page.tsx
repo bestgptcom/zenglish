87b3e12ec067166c6bd1638bbaa80f6b37d85a87
Catalin <151016750+bestgptcom@users.noreply.github.com>
2025-06-27 16:36:01 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import HeaderSection from "@/components/sections/HeaderSection";
 import FeatureSection from "@/components/sections/FeatureSection";
 import CtaSection from "@/components/sections/CtaSection";
 import FAQSection from "@/components/sections/FaqSection";
 import ReviewSection from "@/components/sections/ReviewSection";
+import EmailPopup from "@/components/EmailPopup";
+import { usePopup } from "@/contexts/PopupContext";
 
 export default function Home() {
+  const { isEmailPopupOpen, closeEmailPopup } = usePopup();
+
   return (
     <>
       <HeaderSection />
@@ -12,6 +18,7 @@ export default function Home() {
       <ReviewSection />
       <FAQSection />
       <CtaSection />
+      <EmailPopup isOpen={isEmailPopupOpen} onClose={closeEmailPopup} />
     </>
   );
 }
